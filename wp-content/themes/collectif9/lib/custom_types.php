a36f767ec495ff6854c1dd2972e9678b4af0e4fe
Aurelien Girard <contact@aureliengirard.com>
2021-04-26 20:00:33 +0300

    initial theme files

---
+++ wp-content/themes/collectif9/lib/custom_types.php
@@ -0,0 +1,29 @@
+<?php
+
+// CUSTOM POST TYPES DECLARATION.
+// META BOXES FOR PARENT RELATIONSHIPS ARE AT THE BOTTOM
+
+// Services
+// $labels = array(
+// 	'name' => __('Services'),
+// 	'singular_name' => __('Service'),
+// 	'add_new_item' => __('Add New Service'),
+// 	'new_item' => __('New Service'),
+// );
+// $rewrite = array(
+// 	'slug' => 'service',
+// 	'with_front' => true,
+// 	'pages' => true,
+// );
+// $args = array(
+// 	'labels' => $labels,
+// 	'public' => true,
+// 	'has_archive' => false,
+// 	'show_in_menu' => true,
+// 	'hierarchical' => false,
+// 	'menu_position' => 5,
+// 	'menu_icon' => 'dashicons-hammer',
+// 	'supports' => array('title', 'editor', 'thumbnail', ),
+// 	'rewrite' => $rewrite,
+// );
+// register_post_type('service', $args);
---
+++ wp-content/themes/collectif9/src/js/vendor.js
@@ -0,0 +1,5 @@
+import "jquery";
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+/* Bootstrap */
+import 'bootstrap';
